23472379a7a909ed5b3bd525a1690e139d272cb2
liling yang <yangll2525@163.com>
2022-09-25 18:10:07 -0700

    9

--- 1-50/1 Two Sum.py
+++ 1-50/1 Two Sum.py
@@ -2,13 +2,12 @@ from typing import List
 
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = {}
-        for i, n in enumerate(nums):
-            t = target - n
-            if t in d.keys():
-                return [i, d[t]]
-            else:
-                d[n] = i
-        return []
-
+  def twoSum(self, nums: List[int], target: int) -> List[int]:
+    d = {}
+    for i, n in enumerate(nums):
+      t = target - n
+      if t in d.keys():
+        return [i, d[t]]
+      else:
+        d[n] = i
+    return []
---
+++ 1-50/9 Palindrome Number.py
@@ -0,0 +1,8 @@
+class Solution:
+  def isPalindrome(self, x: int) -> bool:
+    return ''.join(reversed(str(x))) == ''.join(str(x))
+
+  def isPalindrome2(self, x: int) -> bool:
+    x_str = str(x)
+    return x_str == x_str[::-1]
+
